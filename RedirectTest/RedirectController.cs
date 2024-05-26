3969cc186436296e4371545b599dfabb64efe61d
behzad dara <Behzad.dara.99@gmail.com>
2024-05-26 21:21:52 +0330

    Add project files.

---
+++ RedirectTest/RedirectController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RedirectTest;
+
+[ApiController]
+[Route("[controller]")]
+//[EnableCors]
+public class RedirectController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult RedirectToGoogle()
+    {
+        return Redirect("https://www.google.com");
+    }
+}
